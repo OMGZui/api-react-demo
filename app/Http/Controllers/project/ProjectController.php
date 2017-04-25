7c42d61835072a951b6e21879444ace4eb4f53c7
shengj <shengj@fangde.com>
2017-04-25 23:16:09 +0800

    跨域

--- app/Http/Controllers/project/ProjectController.php
+++ app/Http/Controllers/project/ProjectController.php
@@ -19,13 +19,7 @@ class ProjectController extends Controller
     public function index()
     {
         $list = $this->project->getList();
-        return response(['code' => 200, 'result' => $list, 'msg' => '获取成功'])
-            ->withHeaders([
-                'Access-Control-Allow-Origin' => '*',
-                'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, HEAD, OPTIONS',
-                'Access-Control-Allow-Headers' => 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept,Acc-Token'
-            ])
-            ;
+        return response(['code' => 200, 'result' => $list, 'msg' => '获取成功']);
 
     }
 }
